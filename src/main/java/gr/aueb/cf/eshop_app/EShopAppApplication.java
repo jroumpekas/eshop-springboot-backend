af3651432a8e70abfb8fddd5b7e8571a35c89074
Dim.Roumpekas <jimroumpi@gmail.com>
2026-06-09 10:40:06 +0300

    Initial Spring Boot backend setup

---
+++ src/main/java/gr/aueb/cf/eshop_app/EShopAppApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eshop_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShopAppApplication.class, args);
+	}
+
+}
